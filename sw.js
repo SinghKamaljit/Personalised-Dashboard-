110aafd8ba93dc1d4b7c1b740643a8e2b7fccdaa
SinghKamaljit <kamal310i@gmail.com>
2026-06-25 17:51:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smart-dashboard-v4';
+const CACHE='smart-dashboard-v5';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
  e.waitUntil(
